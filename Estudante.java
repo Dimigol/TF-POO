2b2ee626a438979e0a11826d0d8a733c04a7cdef
icoscelia <frederico.celia@edu.pucrs.br>
2026-06-17 10:40:00 -0300

    Classe interface usuario e algumas modificações na classe estudante

--- Estudante.java
+++ Estudante.java
@@ -1,6 +1,49 @@
+import java.util.Set;
+
 class Estudante extends Cliente {
     // 1 placa apenas
-    double saldo; // pode ficar negativo
+    private double saldo; // pode ficar negativo
+
+    public Estudante(String id, String nome, Set<String> placas, double saldo){
+        super(id, nome, placas);
+        this.saldo = saldo;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    @Override
+    double calcularCusto(RegistroEstacionamento r) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    boolean podeEntrar(String placa, Set<String> placasEstacionadas) {
+        // TODO Auto-generated method stub
+        return false;
+    }
     // valor fixo por ingresso/dia, novo ingresso se sair após meia-noite
     // bloqueado se saldo < 0
 }
+
+
+/*abstract class Cliente {
+  - id : String
+  - nome : String
+  - placas : Set<String>
+  + {abstract} calcularCusto(r: RegistroEstacionamento) : double
+  + {abstract} podeEntrar(placa: String, placasEstacionadas: Set<String>) : boolean
+  + adicionarPlaca(placa: String) : boolean
+  + removerPlaca(placa: String) : boolean
+}
+  
+class Estudante {
+  - cpf : String
+  - saldo : double
+  + calcularCusto(r: RegistroEstacionamento) : double
+  + podeEntrar(placa: String, placasEstacionadas: Set<String>) : boolean
+  + debitar(valor: double) : void
+  + estaBloqueado() : boolean
+}*/
---
+++ InterfaceUsuario.java
@@ -0,0 +1,19 @@
+public class InterfaceUsuario {
+    private Estacionamento estacionamento;
+
+    public InterfaceUsuario(Estacionamento estacionamento) {
+        this.estacionamento = estacionamento;
+    }
+
+    public void telaCadastroCliente() {
+        // Implementar a interface de cadastro de clientes
+    }
+
+    public void telaEntradaSaida() {
+        // Implementar a interface de entrada e saída de veículos
+    }
+
+    public void telaRelatorios() {
+        // Implementar a interface de relatórios do estacionamento
+    }
+}
