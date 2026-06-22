13c33565758c1da421a972bf09b16f3ee2fe6e30
icoscelia <frederico.celia@edu.pucrs.br>
2026-06-22 11:16:54 -0300

    descontoClienteFrequente

--- DescontoClienteFrequente.java
+++ DescontoClienteFrequente.java
@@ -1,3 +1,24 @@
+import java.time.LocalDateTime;
+import java.util.List;
+
 class DescontoClienteFrequente implements Desconto {
+
+    @Override
+    public double aplicar(String placa, double valorOriginal, List<RegistroEstacionamento> historico, LocalDateTime hoje) {
+        LocalDateTime TresDiasAtras = hoje.minusDays(3);
+        boolean tem = historico.stream()
+            .anyMatch(r->r.getPlaca().equals(placa) && r.getSaida().isAfter(TresDiasAtras));
+        if (!tem) return 0.0;
+        else{
+            // calcula desconto
+            return 67.0;
+        }
+    }
+
+    @Override
+    public String getId() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getId'");
+    }
     // 10% se usou nos últimos 3 dias
 }
